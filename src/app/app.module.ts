5c02fbac9273c429b757fbadbd5b76adb16f233d
Alessandro Tulioli <desmecad@hotmail.com>
2020-06-11 19:47:31 -0300

    Commit 3 - Preparação para Grid Próximas Lives e ajustes para responsavidade da aplicação em outros tipos de tela

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './views/home/home.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatTabsModule} from '@angular/material/tabs';
+import {MatCardModule} from '@angular/material/card';
 import { LiveListComponent } from './views/home/live-list/live-list.component';
 
 @NgModule({
@@ -22,7 +23,8 @@ import { LiveListComponent } from './views/home/live-list/live-list.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    MatTabsModule
+    MatTabsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
